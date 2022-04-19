bffa9705853f88eea1280f696fd65682f5fa0bbd
Iago Gomes <iagogb@alu.ufc.br>
2022-04-18 21:50:12 -0300

    Commit Inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Card from "./components/Card/Card"
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
+
